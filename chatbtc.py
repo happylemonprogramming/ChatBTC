aecdf19f3400895807c402f447c27b8df43239e3
lemonknowsall <lemonknowsall@gmail.com>
2023-05-12 18:10:51 -0700

    removed qrcode text for debugging

--- chatbtc.py
+++ chatbtc.py
@@ -75,9 +75,9 @@ def sms_reply():
         
         # Send lightning address
         reply = resp.message(lnaddress)
-        # Add a picture message (.jpg, .gif)
-        reply.media(link)
-        
+        # # Add a picture message (.jpg, .gif)
+        # reply.media(link)
+
         # Open subprocess to see if message gets paid
         subprocess.Popen(["python", "checkinvoice.py", payment_hash, from_number])
 
